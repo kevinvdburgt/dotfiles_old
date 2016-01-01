6420290c849d7fe10b8f7d0edaec7af442012b48
Kevin van der Burgt <kevin@zdev.com>
2016-01-01 20:30:44 +0100

    Added compile from source and lemonbar setup

---
+++ .config/bspwm/lemonbar/lemonbar.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+LEMONBAR_FIFO=/tmp/lemonbar-fifo
+LEMONBAR_HEIGHT=16
+LEMONBAR_FOREGROUND="#FFFFFFFF"
+LEMONBAR_BACKGROUND="#44FFFFFF"
+LEMONBAR_FONT="-benis-lemon-abunai-abunai-medium-r-normal--10-110-75-75-m-50-iso10646-1"
+LEMONBAR_MONITORCOUNT=$(bspc query -M | wc -l)
+
+# Check if the lemonbar is running already
+if [ $(pgrep -cx lemonbar.sh) -gt 1 ]; then
+  printf "%s\n" "The lemonbar is already running." >&2
+  exit 1
+fi
+
+# Trap the lemonbar
+trap 'trap - TERM; kill 0' INT TERM QUIT EXIT
+
+# Remove the old fifo file if it still exists, then create a new one
+[ -e "$LEMONBAR_FIFO" ] && rm "$LEMONBAR_FIFO"
+mkfifo "$LEMONBAR_FIFO"
+
+# Tell BSPWM what we want to add a padding to our WM and subscribe the events
+bspc config top_padding $LEMONBAR_HEIGHT
+bspc subscribe report > "$LEMONBAR_FIFO" &
+
+# Subscribe the xtitle process to the fifo file
+xtitle -sf 'XTI%s' > "$LEMONBAR_FIFO" &
+
+# Keep the bar updated every second
+while true; do
+  printf "%s%s\n" "DAT" "$(date +'%d-%m-%y %H:%M:%S')" > "$LEMONBAR_FIFO"
+  printf "%s%s\n" "VOL" "$(volumecontrol get)" > "$LEMONBAR_FIFO"
+  sleep 1s
+done &
+
+# Execute the fifo file to the lemonbar
+cat "$LEMONBAR_FIFO" | \
+  `dirname $0`/lemonbar_display.sh $LEMONBAR_MONITORCOUNT | \
+  lemonbar \
+    -g x$LEMONBAR_HEIGHT \
+    -F "$LEMONBAR_FOREGROUND" \
+    -B "$LEMONBAR_BACKGROUND" \
+    -f "$LEMONBAR_FONT" \
+    -p
+
+wait
---
+++ .config/bspwm/lemonbar/lemonbar_display.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+SCREEN_COUNT=$1
+SCREEN_OFFSET=' '
+
+while read -r line; do
+  case $line in
+
+    # Update the date and time
+    DAT*)
+      VAR_DAT="${line#???}"
+      ;;
+
+    # Update the focussed window title
+    XTI*)
+      VAR_XTI="${line#???}"
+      ;;
+
+    # Update the audio volume
+    VOL*)
+      VAR_VOL="${line#???}"
+      ;;
+
+    # BSPWM Related
+    W*)
+      wminfo=("" "" "")
+      currentMonitor=0
+      bspwmMessage=${line#?}
+
+      IFS=':'
+      set -- ${line#?}
+      while [ $# -gt 0 ]; do
+        item=$1
+        name=${item#?}
+        case $item in
+
+          # Current monitor
+          [Mm]*)
+            currentMonitor="$name"
+            ;;
+
+            # Focussed occupied desktop
+            O*)
+              wminfo[currentMonitor]="${wminfo[$currentMonitor]}%{+u} ${name} %{-u}"
+              ;;
+
+            # Focussed free desktop
+            F*)
+              wminfo[currentMonitor]="${wminfo[$currentMonitor]}%{+u} ${name} %{-u}"
+              ;;
+
+            # Focussed urgent desktop
+            U*)
+              wminfo[currentMonitor]="${wminfo[$currentMonitor]}%{+u} ${name} %{-u}"
+              ;;
+
+            # Occupied desktop
+            o*)
+              wminfo[currentMonitor]="${wminfo[$currentMonitor]} ${name} "
+              ;;
+
+            # Free desktop
+            f*)
+              wminfo[currentMonitor]="${wminfo[$currentMonitor]} ${name} "
+              ;;
+
+            # Urgent desktop
+            u*)
+              wminfo[currentMonitor]="${wminfo[$currentMonitor]} ${name} "
+              ;;
+
+        esac
+        shift
+      done
+      ;;
+  esac
+
+  if [ $SCREEN_COUNT -eq 1 ]; then
+    printf "%s" "TODO: Fetch info"
+  elif [ $SCREEN_COUNT -eq 2 ]; then
+    printf "%s" "TODO: Fetch info"
+  elif [ $SCREEN_COUNT -eq 3 ]; then
+    # Screen 1 (left)
+    printf "%s" "%{S0}%{l}${SCREEN_OFFSET}"
+    printf "%s" "${wminfo[0]}"
+
+    # Screen 1 (right)
+    printf "%s" "%{S0}%{r}"
+    printf "%s" ">>> ZDEV 1 <<<"
+
+    # Screen 2 (left)
+    printf "%s" "${SCREEN_OFFSET}%{S1}%{l}${SCREEN_OFFSET}"
+    printf "%s" "${wminfo[1]}- $VAR_XTI"
+
+    # Screen 2 (right)
+    printf "%s" "%{S1}%{r}"
+    printf "%s" "Battery: 100%   Volume: $VAR_VOL%   Date: $VAR_DAT"
+
+    # Screen 3 (left)
+    printf "%s" "${SCREEN_OFFSET}%{S2}%{l}${SCREEN_OFFSET}"
+    printf "%s" "${wminfo[2]}"
+
+    # Screen 3 (right)
+    printf "%s" "%{S2}%{r}"
+    printf "%s" ">>> ZDEV 3 <<<"
+  fi
+
+  printf "%s\n" "${SCREEN_OFFSET}"
+
+done
