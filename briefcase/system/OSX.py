cc748ac0e43d0b880f2e8ae1dddd6aae3e65ab47
Kevin van der Burgt <kevin@zdev.com>
2016-01-03 21:53:11 +0100

    Added killdock to perform the change

--- briefcase/system/OSX.py
+++ briefcase/system/OSX.py
@@ -13,5 +13,6 @@ class SyncOSX:
         localset_wallpaper_remote_url = briefcase.get('localset', 'wallpaper', 'remote_url')
         if settings_wallpaper_remote_url != localset_wallpaper_remote_url:
             briefcase.shell('curl ' + settings_wallpaper_remote_url + ' -o wallpaper.jpg', True, '~/.config/');
-            print('osascript -e \'tell application "Finder" to set desktop picture to POSIX file "' + os.path.expanduser('~/.config/wallpaper.jpg') + '"\'')
+            briefcase.shell('osascript -e \'tell application "Finder" to set desktop picture to POSIX file "' + os.path.expanduser('~/.config/wallpaper.jpg') + '"\'')
+            briefcase.shell('killall Dock')
             briefcase.set('localset', 'wallpaper', 'remote_url', settings_wallpaper_remote_url)
