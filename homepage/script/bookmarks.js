3b707f8cdb08546b6dbbe20c5a35f069ae7b7f7c
Kevin van der Burgt <kevin@zdev.com>
2016-10-02 22:14:23 +0200

    Added the bookmark script

---
+++ homepage/script/bookmarks.js
@@ -0,0 +1,30 @@
+if (!bookmarks) {
+  var bookmarks = [
+    {
+      'title': 'You can create your own bookmars, click here to learn more!',
+      'href': 'https://github.com/kevinvdburgt/dotfiles/homepage',
+    }
+  ];
+}
+
+function bookmarksCreate(element, data) {
+  var list = document.createElement('ul');
+  element.appendChild(list);
+
+  data.forEach(function (entry) {
+    var item = document.createElement('li');
+    list.appendChild(item);
+
+    var link = document.createElement('a');
+    item.appendChild(link);
+
+    link.setAttribute('href', entry.href);
+    link.appendChild(document.createTextNode(entry.title));
+
+    if (entry.sub && entry.sub.length > 0) {
+      bookmarksCreate(item, entry.sub);
+    }
+  });
+}
+
+bookmarksCreate(document.querySelector('nav.bookmarks'), bookmarks);
